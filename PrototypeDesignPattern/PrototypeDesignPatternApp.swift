60144802d528462f7135f8a39efc6e46a6aadede
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2024-08-22 21:06:29 +0300

    Initial Commit

---
+++ PrototypeDesignPattern/PrototypeDesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeDesignPatternApp.swift
+//  PrototypeDesignPattern
+//
+//  Created by Ramill Ibragimov on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeDesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
